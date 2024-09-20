5be87b991619a2e21ec3aa54787dd218ad61c359
Иван <dark-fighter@mail.ru>
2024-09-20 06:56:21 +0300

    Исправление проверки длины списка

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,7 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, 7, 9, 8, 7, 6, 5]
 n = 0
-while n <= len(my_list):
+while n < len(my_list):
 	if my_list[n] > 0:
 		print(my_list[n])
 		n += 1
