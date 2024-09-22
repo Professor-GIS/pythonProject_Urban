47764dc5eb9c3136599b0cdf4ee2c40093d01eaf
Иван <dark-fighter@mail.ru>
2024-09-22 10:39:27 +0300

    корректировка module_2_5_hard

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,4 +4,4 @@ for i in range(1, (n // 2) + 1):
 	for j in range(i + 1, n):
 		if n % (i + j) == 0:
 			result += f'{i}{j}'
-print("Второе число:", int(result))
+print("Второе число =", int(result))
