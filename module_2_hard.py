ce2ccaeaa8d40f4d13a1059faa82ff1026955d8a
Иван <dark-fighter@mail.ru>
2024-09-22 10:34:42 +0300

    Публикация module_2_5_hard

---
+++ module_2_hard.py
@@ -0,0 +1,7 @@
+n = int(input("Ведите первое число (3-20): "))
+result = ""
+for i in range(1, (n // 2) + 1):
+	for j in range(i + 1, n):
+		if n % (i + j) == 0:
+			result += f'{i}{j}'
+print(int(result))
