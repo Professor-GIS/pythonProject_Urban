a3fac8ce3a010cc0b6f197a668f0c05e020b703a
Иван <dark-fighter@mail.ru>
2024-10-03 20:10:16 +0300

    Updating module_3_hard

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,28 +1,28 @@
-def calculate_structure_sum(data, summ_=None):
-	if isinstance(data, int|float):
-		summ_ += data
+def calculate_structure_sum(data):
+	if isinstance(data, int):
+		return data
 	elif isinstance(data, str):
-		summ_ += len(data)
-	elif isinstance(data, list):
-		if len(data) ==1:
-			
-		if isinstance(i, list):
-			for j in i:
-
-	return summ_
-
-
+		return len(data)
+	elif isinstance(data, list | tuple):
+		if len(data) == 0:
+			return 0
+		if len(data) == 1:
+			return calculate_structure_sum(data[0])
+		else:
+			return calculate_structure_sum(data[0]) + calculate_structure_sum(data[1:])
+	elif isinstance(data, set):
+		return calculate_structure_sum(list(data))
+	elif isinstance(data, dict):
+		return calculate_structure_sum(list(data.keys())) + calculate_structure_sum(list(data.values()))
 
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+	[1, 2, 3],
+	{'a': 4, 'b': 5},
+	(6, {'cube': 7, 'drum': 8}),
+	"Hello",
+	((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 result = calculate_structure_sum(data_structure)
 print(result)
-
-print(len('a'))
