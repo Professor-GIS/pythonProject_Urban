5d534d50412eea670df6f29b27611852ed336f9e
Иван <dark-fighter@mail.ru>
2024-11-08 10:34:23 +0300

    Публикация_2 module_7_5

--- module_7_5.py
+++ module_7_5.py
@@ -1,4 +1,4 @@
-#Домашнее задание по теме "Файлы в операционной системе"
+# Домашнее задание по теме "Файлы в операционной системе"
 import os
 import time
 
@@ -11,7 +11,11 @@ def folder_info(directory):
 			formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 			filesize = os.path.getsize(filepath)
 			parent_dir = os.path.dirname(filepath)
-			print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+			print(
+				f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
+				f'Родительская директория: {parent_dir}'
+			)
+
 
 directory = input("Путь к каталогу для обхода: ")
 folder_info(directory)
