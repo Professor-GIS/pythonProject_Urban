19513d8a5a32a9331bdcc6c6aed43918935dfd01
Иван <dark-fighter@mail.ru>
2024-11-10 15:16:32 +0300

    _

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+class Figure:
+	sides_count = 0
 
+	def __init__(self, __color: list, __sides: list, filled=True):
+		self.__color = list(__color)
+		self.__sides = list(__sides) #тут нужна проверка на количество и правильность сторон
+
+class Circle(Figure):
+	sides_count = 1
+
+	def __init__(self, color, *__sides, filled=True, __radius=0):
+		super().__init__(color, __sides)
+		self.__radius = __sides[0] / (2 * pi)
