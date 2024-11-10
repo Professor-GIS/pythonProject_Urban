09927f640f3f28eba1077ff2b74af2a48848eef6
Иван <dark-fighter@mail.ru>
2024-11-10 11:19:18 +0300

    Публикация модуля_8_1

---
+++ Module_8_1.py
@@ -0,0 +1,14 @@
+# Домашнее задание по теме "Try и Except"
+
+def add_everything_up(a, b):
+	try:
+		x = round(a + b, 3)
+	except TypeError:
+		return f'{a}{b}'
+	else:
+		return x
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
