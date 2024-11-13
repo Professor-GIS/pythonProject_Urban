87d15fd6cac1c4bce7dd2b0f5a38264e85ebca5d
Иван <dark-fighter@mail.ru>
2024-11-13 11:17:54 +0300

    Корректировка module_8_3

--- module_8_3.py
+++ module_8_3.py
@@ -4,26 +4,26 @@
 class Car:  # Создаём экземпляр класса
 	def __init__(self, model: str, __vin: int, __numbers: str):
 		self.model = model
-		if self.__is_valid_vin(__vin):  # Проверяем соответствует ли vin требованиям через специальный метод
-			self.__vin = __vin  # Если соответствует - задаём инкапсулированный атрибут __vin
-		if self.__is_valid_numbers(__numbers):  # Проверяем соответствует ли номер требованиям через специальный метод
-			self.__numbers = __numbers  # Если соответствует - задаём инкапсулированный атрибут __numbers
+		if self.__is_valid_vin(__vin):
+			self.__vin = __vin
+		if self.__is_valid_numbers(__numbers):
+			self.__numbers = __numbers
 
-	def __is_valid_vin(self, vin_number):  # Метод проверяющий vin на соответствие требованиям
-		if not isinstance(vin_number, int):  # Если тип данных НЕ int - выдаём ошибку с сообщением
-			raise IncorrectVinNumber('Некорректный тип vin номер')
+	def __is_valid_vin(self, vin_number):
+		if not isinstance(vin_number, int):
+			raise IncorrectVinNumber('Некорректный тип vin номер: vin должен быть целым числом (int)')
 		else:
-			if 1000000 > vin_number < 9999999:  # Проверяем диапазон vin, иначе выдаём ошибку с сообщением
-				raise IncorrectVinNumber('Неверный диапазон для vin номера')
-		return True  # Если проверки прошли без ошибок - возвращаем True
+			if 1000000 > vin_number < 9999999:
+				raise IncorrectVinNumber('Неверный диапазон для vin номера: vin должен быть в диапазоне 1000000-9999999')
+		return True
 
 	def __is_valid_numbers(self, car_numbers):
-		if not isinstance(car_numbers, str):  # Если тип данных НЕ str - выдаём ошибку с сообщением
-			raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+		if not isinstance(car_numbers, str):
+			raise IncorrectCarNumbers('Некорректный тип данных для номеров: номер должен быть строковым значением (str)')
 		else:
-			if not len(car_numbers) == 6:  # Если длинна номера НЕ 6 знаков - выдаём ошибку с сообщением
-				raise IncorrectCarNumbers('Неверная длина номера')
-		return True  # Если проверки прошли без ошибок - возвращаем True
+			if not len(car_numbers) == 6:
+				raise IncorrectCarNumbers('Неверная длина номера: номер должен состоять из 6 знаков')
+		return True
 
 
 class IncorrectVinNumber(Exception):
