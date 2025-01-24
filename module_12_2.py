b9d111ec4af89b89c8d8fd13ef94ef6db0838bb9
Иван <dark-fighter@mail.ru>
2025-01-24 21:55:05 +0300

    Публикация module_13_6

--- module_12_2.py
+++ module_12_2.py
@@ -5,7 +5,6 @@ import unittest
 
 
 class TournamentTest(unittest.TestCase):
-	is_frozen = True
 	
 	@classmethod
 	def setUpClass(cls):
@@ -21,19 +20,16 @@ class TournamentTest(unittest.TestCase):
 		for i in cls.all_results:
 			print(cls.all_results[i])
 	
-	@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
 	def test_1(self):
 		t_1 = Tournament(90, (self.r_1, self.r_3))
 		self.all_results[1] = t_1.start()
 		self.assertTrue((self.all_results[1])[2] == 'Ник')
 	
-	@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
 	def test_2(self):
 		t_2 = Tournament(90, (self.r_2, self.r_3))
 		self.all_results[2] = t_2.start()
 		self.assertTrue((self.all_results[2])[2] == 'Ник')
 	
-	@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
 	def test_3(self):
 		t_3 = Tournament(90, (self.r_1, self.r_2, self.r_3))
 		self.all_results[3] = t_3.start()
